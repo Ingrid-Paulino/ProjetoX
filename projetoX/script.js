44db8675f7030984d41688510d7a899b410ee5d7
Ingrid-Paulino <https://github.com/Ingrid-Paulino>
2021-05-01 20:16:30 -0300

    Atualização

---
+++ projetoX/script.js
@@ -0,0 +1,8 @@
+function mat() {
+    var inMat = document.getElementById('mat');
+
+    var mat = inMat.value;
+    var entrar = 
+}
+var btmat = document.getElementById('btmat');
+btmat.addEventListener("click", mat);
